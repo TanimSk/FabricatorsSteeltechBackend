1e4d86e735779f91a2723cbafef518947706babe
tanimsk <sktanim5800@gmail.com>
2025-06-23 11:29:13 +0600

    report

--- server/marketing_rep/urls.py
+++ server/marketing_rep/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from marketing_rep.views import ReportsView
 
 
-urlpatterns = []
+urlpatterns = [
+    path("reports/", ReportsView.as_view(), name="reports"),
+]
--- server/marketing_rep/views.py
+++ server/marketing_rep/views.py
@@ -1,3 +1,76 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.response import Response
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.permissions import BasePermission
+from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
+from dj_rest_auth.registration.views import RegisterView
+from django.db.models import Sum
+from django.http import HttpResponse
+from django.utils import timezone
+import requests
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.utils.crypto import get_random_string
+import csv
 
-# Create your views here.
+
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+
+# models
+from marketing_rep.models import Reports
+from fabricator.models import Fabricator
+
+# serializers
+from marketing_rep.serializers import ReportsSerializer
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 500
+    page_query_param = "p"
+
+
+# Authenticate User Only Class
+class AuthenticateOnlyMar(BasePermission):
+    def has_permission(self, request, view):
+        if not request.user or not request.user.is_authenticated:
+            raise PermissionDenied("User is not authenticated.")
+
+        if not getattr(request.user, "is_marketing_representative", False):
+            raise PermissionDenied("User is not an Marketing Representative.")
+
+        return True
+
+
+class ReportsView(APIView):
+    permission_classes = [AuthenticateOnlyMar]
+
+    def get(self, request, *args, **kwargs):
+        """
+        List of fabricators and distributors for option list.
+        """
+        if request.query_params.get("view") == "fabricators":
+            fabricators = Fabricator.objects.filter(
+                marketing_representative=request.user.marketingrepresentative
+            ).values(
+                "id",
+                "name",
+                "registration_number",
+                "distributor__name",
+            )
+            return Response(fabricators, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        """
+        Create a new report.
+        """
+        serializer = ReportsSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(marketing_rep=request.user.marketingrepresentative)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
