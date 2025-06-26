e610eeb192751caaa177b8d73b0c2bcda479c25b
tanimsk <sktanim5800@gmail.com>
2025-06-26 10:49:01 +0600

    feedbacks

--- server/marketing_rep/admin.py
+++ server/marketing_rep/admin.py
@@ -2,15 +2,4 @@ from django.contrib import admin
 from .models import Reports
 
 
-@admin.register(Reports)
-class ReportsAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "marketing_rep",
-        "fabricator",
-        "distributor",
-        "amount",
-        "invoice_number",
-        "sales_date",
-        "created_at",
-    )
+admin.site.register(Reports)
