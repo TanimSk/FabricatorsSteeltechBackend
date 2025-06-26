aa62a86257a1bd92826ed28e0ba5eeeadbf53565
tanimsk <sktanim5800@gmail.com>
2025-06-26 09:47:18 +0600

    feedbacks

--- server/fabricator/admin.py
+++ server/fabricator/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Fabricator
+
+@admin.register(Fabricator)
+class FabricatorAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'institution', 'phone_number', 'registration_number',
+        'district', 'sub_district', 'status', 'created_at'
+    )
+    search_fields = ('name', 'institution', 'phone_number', 'registration_number')
+    list_filter = ('status', 'district', 'sub_district')
+    ordering = ('-created_at',)
 
 # Register your models here.
