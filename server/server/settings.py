7e3ed980da02c4802d3a92771685231ba24a39a4
tanimsk <sktanim5800@gmail.com>
2025-06-28 05:51:17 +0600

    db

--- server/server/settings.py
+++ server/server/settings.py
@@ -113,19 +113,19 @@ WSGI_APPLICATION = "server.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
-    # postgres example
     # "default": {
-    #     "ENGINE": "django.db.backends.postgresql",
-    #     "NAME": "travonus",
-    #     "USER": os.getenv("USERNAME_DB"),
-    #     "PASSWORD": os.getenv("PASSWORD_DB"),
-    #     "HOST": "localhost",
-    #     "PORT": "",
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
     # },
+    # postgres example
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "travonus",
+        "USER": os.getenv("USERNAME_DB"),
+        "PASSWORD": os.getenv("PASSWORD_DB"),
+        "HOST": "localhost",
+        "PORT": "",
+    },
 }
 
 
