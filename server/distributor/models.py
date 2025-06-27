df1538028fc7d293d142a26cec64b1d7d9d20e03
tanimsk <sktanim5800@gmail.com>
2025-06-27 16:10:06 +0600

    dist email blank true

--- server/distributor/models.py
+++ server/distributor/models.py
@@ -16,7 +16,7 @@ class Distributor(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=512)
     phone_number = models.CharField(max_length=20)
-    email = models.EmailField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, blank=True, null=True)
     district = models.CharField(max_length=255)
     sub_district = models.CharField(max_length=255)    
     created_at = models.DateTimeField(auto_now_add=True)    
