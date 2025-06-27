f36d1e2da2fa12f2f871283a3f089cf30dbb44da
tanimsk <sktanim5800@gmail.com>
2025-06-27 13:21:38 +0600

    product selling

--- server/marketing_rep/models.py
+++ server/marketing_rep/models.py
@@ -100,7 +100,7 @@ class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return self.description
 
 
 class RecentActivity(models.Model):
