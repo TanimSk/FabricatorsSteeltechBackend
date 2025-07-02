512d4485983164a9500bf3a5348c7f053fbfdbdb
tanimsk <sktanim5800@gmail.com>
2025-07-02 22:10:57 +0600

    sms added

--- server/server/settings.py
+++ server/server/settings.py
@@ -203,7 +203,8 @@ REST_USE_JWT = True
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = DEFAULT_FROM_EMAIL = "Steeltech <noreply.service.tanimsk@gmail.com>"
+EMAIL_HOST_USER = "noreply.service.tanimsk@gmail.com"
+DEFAULT_FROM_EMAIL = "Steeltech <noreply.service.tanimsk@gmail.com>"
 EMAIL_HOST_PASSWORD = "igcosdywfsqodffg"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
@@ -266,4 +267,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Load environment variables
 FRONTEND_URL = os.getenv("FRONTEND_URL")
 TRANSFER_ONGSHAK_API_KEY = os.getenv("TRANSFER_ONGSHAK_API_KEY")
-CLOUD_SMS_API_KEY = os.getenv("CLOUD_SMS_API_KEY")
+CLOUDSMSBD_API_KEY = os.getenv("CLOUD_SMS_API_KEY")
