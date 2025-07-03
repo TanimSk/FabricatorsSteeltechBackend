e02422ded186ec973a4119ab6c1ba4bea03b9261
tanimsk <sktanim5800@gmail.com>
2025-07-03 19:38:44 +0600

    admin

--- server/distributor/admin.py
+++ server/distributor/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from distributor.models import Distributor
 
+
 @admin.register(Distributor)
 class DistributorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone', 'district', 'created_at')
-    search_fields = ('name', 'email', 'phone')    
+    list_display = ("name", "email", "phone_number", "district", "created_at")
+    search_fields = ("name", "email", "phone_number")
