2491700dc15e6168af1c9a56ba65ad2e65e7fb6c
tanimsk <sktanim5800@gmail.com>
2025-07-05 17:34:07 +0600

    sms

--- server/marketing_rep/admin.py
+++ server/marketing_rep/admin.py
@@ -3,4 +3,18 @@ from .models import Reports, MarketingRepresentative
 
 
 admin.site.register(Reports)
-admin.site.register(MarketingRepresentative)
+
+@admin.register(MarketingRepresentative)
+class MarketingRepresentativeAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "phone_number",
+        "email",
+        "district",
+        "sub_district",
+        "created_at",
+    )
+    search_fields = ("name", "phone_number", "email", "district", "sub_district")
+    list_filter = ("district", "sub_district")
+    ordering = ("-created_at",)
+    list_per_page = 20
